e67c14275bcbf57a02848e74768717977c59da5a
DestroAmer2020 <teleskovladislav@gmail.com>
2025-12-04 17:11:58 +0200

    Initial commit

--- scriptthree.sh
+++ scriptthree.sh
@@ -8,7 +8,8 @@ apt-get update -y
 
 # === Create adminuser securely ===
 useradd -m -s /bin/bash adminuser
-echo "adminuser:$(openssl passwd -6 AdminP@ssw0rd)" | chpasswd -e
+HASH=$(openssl passwd -6 'AdminP@ssw0rd')
+echo "adminuser:${HASH}" | chpasswd -e
 
 # Give sudo access
 usermod -aG sudo adminuser
@@ -21,9 +22,9 @@ passwd -d poweruser
 echo "poweruser ALL=(ALL) NOPASSWD: /usr/sbin/iptables" > /etc/sudoers.d/poweruser-iptables
 chmod 440 /etc/sudoers.d/poweruser-iptables
 
-# === Allow only poweruser to read adminuser home ===
-setfacl -m u:poweruser:rx /home/adminuser
+# === Allow only poweruser to read adminuser home (via group) ===
+chown adminuser:poweruser /home/adminuser
+chmod 750 /home/adminuser
 
 # === Create symlink ===
 ln -s /etc/mtab /home/poweruser/mtab_link
-chown poweruser:poweruser /home/poweruser/mtab_link
